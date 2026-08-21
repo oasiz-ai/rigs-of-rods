569327e34766c323d50cdf9ebab07fc95d7d8f65
behoyh <beshoyhanna@outlook.com>
2026-08-20 21:48:16 -0700

    physics: include impact setup in stress suite

--- tools/run-physics-tests.sh
+++ tools/run-physics-tests.sh
@@ -90,6 +90,11 @@ fi
 
 "${physics_test_compiler}" \
     "${common_test_flags[@]}" \
+    "${repository_dir}/tests/physics/DeterministicImpactInitialConditionTests.cpp" \
+    -o "${test_build_dir}/deterministic_impact_initial_condition_tests"
+
+"${physics_test_compiler}" \
+    "${common_test_flags[@]}" \
     "${repository_dir}/tests/physics/DeterministicContactOrderTests.cpp" \
     -o "${test_build_dir}/deterministic_contact_order_tests"
 
@@ -173,6 +178,7 @@ physics_test_executables=(
     deterministic_counter_noise_tests
     deterministic_fixed_step_cadence_tests
     deterministic_scenario_schedule_tests
+    deterministic_impact_initial_condition_tests
     deterministic_contact_order_tests
     deterministic_input_trace_tests
     deterministic_input_trace_runtime_tests
